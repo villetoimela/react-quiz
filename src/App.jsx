59e39fb2207b19c1fb1d194e62d7659246eeaa43
Ville Toimela <ville.toimela@hotmail.com>
2021-10-12 13:27:36 +0300

    quiz section and money section placed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+    <div className="main">main</div>
+    <div className="pyramid">money</div>
+    </div>
+  );
+}
+
+export default App;
